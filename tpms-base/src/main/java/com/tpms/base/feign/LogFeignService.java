7459c6e06d7b17f00f97abcb2222075e476a9464
卢炜东 <1606166824@qq.com>
2023-05-23 00:56:53 +0800

    小调整

---
+++ tpms-base/src/main/java/com/tpms/base/feign/LogFeignService.java
@@ -0,0 +1,22 @@
+package com.tpms.base.feign;
+
+import com.tpms.common.web.bean.sys.OperateLog;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @author wld
+ * @date 2023/5/22 - 15:09
+ */
+@ComponentScan
+@FeignClient(name = "tpms-rbac",path = "/sys/log")
+public interface LogFeignService {
+    /**
+     * 记录日志
+     * @param operateLog
+     */
+    @PostMapping("/log")
+    void log(@RequestBody OperateLog operateLog);
+}
